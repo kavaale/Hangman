1bcf628bbd8bff14cbda455cda9532222f312300
kavaale <kavanaughalex@yahoo.com>
2020-12-04 22:17:55 -0600

    Initial commit

---
+++ stuff.js
@@ -0,0 +1,45 @@
+$(function (){
+    $("form").submit(submitChoice);
+
+    var words = ["funny", "submit", "function", "parse", "sweeper", "bleach", "guitar", "world", "metal",
+    "display", "narcissist", "monday", "bones", "witch", "frayed", "powder"];//length = 16
+    var answer = words[(Math.random()*15).toFixed()];
+    console.log(answer);
+    var i = 0;
+    var answerArray = answer.split("");
+    var outputArray = []
+    while(answer.length > i){
+        outputArray.push(" _ ");
+        i++
+    }
+    var output = outputArray.join("");
+    $("#wordConfirm").text(output);
+    var image = 1;
+
+    function submitChoice(event){
+
+        event.preventDefault();
+        i = 0;
+        var choice = $("#choice").val().toLowerCase();
+        answerArray.forEach(function (item, index){
+            if(choice==answerArray[index]){
+                outputArray[index] = item;
+                i++;
+            }
+        })
+        $("input").val("");
+        output = outputArray.join("");
+        $("#wordConfirm").text(output);
+        if(i<1){
+            image++;
+            $("#hangman").prop("src", "Hangman/hangman" + image + ".png")
+        }
+
+    }
+
+    function changePic(event){
+        event.preventDefault();
+        image++;
+        $("#hangman").prop("src", "Hangman/hangman" + image + ".png")
+    }
+})
