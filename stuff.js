13ab32e70498d6259a34dc401154bfd7ced37440
kavaale <kavanaughalex@yahoo.com>
2020-12-04 22:49:50 -0600

    final commit

--- stuff.js
+++ stuff.js
@@ -2,44 +2,41 @@ $(function (){
     $("form").submit(submitChoice);
 
     var words = ["funny", "submit", "function", "parse", "sweeper", "bleach", "guitar", "world", "metal",
-    "display", "narcissist", "monday", "bones", "witch", "frayed", "powder"];//length = 16
+    "display", "narcissist", "monday", "bones", "witch", "frayed", "powder"]; //length = 16
     var answer = words[(Math.random()*15).toFixed()];
-    console.log(answer);
+    console.log(answer); //left in for easy debugging
     var i = 0;
     var answerArray = answer.split("");
-    var outputArray = []
+    var outputArray = [];
     while(answer.length > i){
         outputArray.push(" _ ");
-        i++
+        i++;
     }
     var output = outputArray.join("");
     $("#wordConfirm").text(output);
     var image = 1;
 
     function submitChoice(event){
-
         event.preventDefault();
-        i = 0;
-        var choice = $("#choice").val().toLowerCase();
-        answerArray.forEach(function (item, index){
-            if(choice==answerArray[index]){
-                outputArray[index] = item;
-                i++;
+        if(image<7) {
+            i = 0;
+            var choice = $("#choice").val().toLowerCase();
+            answerArray.forEach(function (item, index) {
+                if (choice == answerArray[index]) {
+                    outputArray[index] = item;
+                    i++;
+                }
+            })
+            $("input").val("");
+            output = outputArray.join("");
+            $("#wordConfirm").text(output);
+            if (i < 1 && image < 7) {
+                image++;
+                $("#hangman").prop("src", "Hangman/hangman" + image + ".png");
+                if(image==7){
+                    $("#wordConfirm").text("You Lose!")
+                }
             }
-        })
-        $("input").val("");
-        output = outputArray.join("");
-        $("#wordConfirm").text(output);
-        if(i<1){
-            image++;
-            $("#hangman").prop("src", "Hangman/hangman" + image + ".png")
         }
-
-    }
-
-    function changePic(event){
-        event.preventDefault();
-        image++;
-        $("#hangman").prop("src", "Hangman/hangman" + image + ".png")
     }
 })
